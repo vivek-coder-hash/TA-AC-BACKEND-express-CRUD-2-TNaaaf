117224a71c6650e3b89ffae4cb122e2ca2d50a0f
vivek sharma <vivekslim@gmail.com>
2021-07-26 16:21:36 +0530

    assignment II

---
+++ block-BNaadm/blog/app.js
@@ -0,0 +1,36 @@
+var express = require("express")
+var mongoose =require("mongoose")
+var path = require("path")
+
+mongoose.connect("mongodb://localhost/blog" , {useNewUrlParser:true , useUnifiedTopology:true} , (err)=> {
+    console.log(err ? err: "connect to database")
+})
+
+
+var app = express()
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//setup view engine
+app.set("view engine" , "ejs" )
+app.set("views" , path.join(__dirname , "views"))
+
+//routing middleware
+app.use("/articles" , require("./routes/blog"))
+//app.use("/comments" , require("./routes/comment"))
+
+//Error handler
+app.use((req,res,next)=> {
+    res.send("Page not found")
+})
+
+app.use((err,req,res,next)=> {
+    res.send(err)
+})
+
+//listen
+app.listen(4000,()=>{
+    console.log("server listening to port 4000")
+})
---
+++ block-BNaadm/blog/routes/blog.js
@@ -0,0 +1,99 @@
+var express = require("express")
+var router = express.Router()
+var Article  = require("../models/article")
+
+
+router.get("/" , (req,res,next)=> {
+    Article.find({} , (err,articles)=> {
+     if(err) return next(err)
+     res.render("articles.ejs" ,  {articles:articles})
+    })
+})
+
+
+// create article form
+router.get("/new" , (req,res)=> {
+    res.render("addArticle.ejs")
+})
+
+
+router.get("/:id" , (req,res,next)=> {
+    var id =req.params.id
+    //handle here
+    
+    Article.findById(id , (err,article)=> {
+        
+        if(err) return next(err)
+        res.render("articleDetails.ejs" , {article:article})
+    })
+})
+
+
+
+
+router.post("/" , (req,res,next)=> {
+    req.body.tags =req.body.tags.trim().split(" ")
+   
+   Article.create(req.body , (err , createdArticle)=> {
+    console.log(req.body)
+       if(err) return next(err)
+       res.redirect("/articles")
+   })
+})
+
+
+//Edit article form
+
+router.get("/:id/edit" , (req,res,next)=> {
+    var id =req.params.id
+    
+    Article.findById(id , (err,article)=> {
+        article.tags =article.tags.join(" ")
+        
+        if(err) return next(err)
+        res.render("editArticleForm.ejs", {article:article}) 
+    })
+})
+
+
+//save updated data
+router.post("/:id" , (req,res,next)=> {
+    var id = req.params.id
+    req.body.tags =req.body.tags.split(" ")
+    Article.findByIdAndUpdate(id,req.body,(err,updatedArticle)=> {
+        
+        if(err) return next(err)
+        res.redirect("/articles/"+id)
+    })
+})
+
+
+//delete article
+router.get("/:id/delete" , (req,res,next)=> {
+    var id =req.params.id
+    Article.findByIdAndDelete(id, (err,article)=> {
+        if(err) return next(err)
+        res.redirect("/articles")
+    })
+})
+
+//increment likes
+router.get("/:id/likes" , (req,res,next)=> {
+    var id =req.params.id
+    Article.findByIdAndUpdate(id , {$inc:{likes:1}} , (err,article)=> {
+        if(err) return next(err)
+        res.redirect("/articles/"+id)
+    })
+
+})
+
+//decrement likes
+router.get("/:id/dislikes" , (req,res,next)=> {
+    var id =req.params.id
+    Article.findByIdAndUpdate(id , {$inc:{likes:-1}} , (err ,article)=> {
+        if(err) return next(err)
+        res.redirect("/articles/"+id)
+    })
+})
+
+module.exports=router
